a89bd09dbe8c52a9fe82e2848c0c85a4c15aa447
ljschfr <ljschaefer04@gmail.com>
2024-10-25 11:57:12 +0200

    Delete public_html directory

--- public_html/js/animation.js
+++
@@ -1,8 +0,0 @@
-/* FremdCode von ChatGPT
-Slide-In-Animation der Seitentitel */
-document.addEventListener("DOMContentLoaded", function() {
-    const slideInElement = document.getElementById("slideIn");
-    setTimeout(() => {
-        slideInElement.classList.add('slide-in-visible');
-    }, 100);
-});
